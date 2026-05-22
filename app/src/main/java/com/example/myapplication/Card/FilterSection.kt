263940c811a3dcf65e7361a83b38b39a6ad6979c
Kimchretien <kimintsinzi@gmail.com>
2026-05-22 10:35:31 +0600

    mis a jour 3

--- app/src/main/java/com/example/myapplication/Card/FilterSection.kt
+++ app/src/main/java/com/example/myapplication/Card/FilterSection.kt
@@ -1,65 +1,91 @@
 package com.example.myapplication.Card
 
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.FilterChip
 import androidx.compose.material3.FilterChipDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.material3.Text
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun FilterSection() {
+    var expanded =remember { mutableStateOf(false) }
 
-    Row {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
 
-        FilterChip(
-            selected = true,
-            onClick = {},
-            label = {
-                Text("Chaussures")
-            },
-            colors = FilterChipDefaults.filterChipColors(
-                selectedContainerColor = Color(0XFF1C69C0),
-                selectedLabelColor = Color.White,
-                containerColor = Color.LightGray,
-                labelColor = Color.Black
+        Row(
+            modifier = Modifier
+                .weight(1f)
+                .horizontalScroll(rememberScrollState())
+                .padding(end = 16.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+            Filterchip("Chaussures")
+            Filterchip("Vetements")
+            Filterchip("Electronique")
+        }
+
+        IconButton(onClick = { expanded.value= true }) {
+            Icon(
+                imageVector = Icons.Default.MoreVert,
+                contentDescription = "More Filtres"
             )
-        )
+        }
 
-        Spacer(modifier = Modifier.width(8.dp))
-
-        FilterChip(
-            selected = true,
-            onClick = {},
-            label = {
-                Text("Vetements")
-            },
-            colors = FilterChipDefaults.filterChipColors(
-                selectedContainerColor = Color(0XFF1C69C0),
-                selectedLabelColor = Color.White,
-                containerColor = Color.LightGray,
-                labelColor = Color.Black
+        DropdownMenu(
+            expanded = expanded.value,
+            onDismissRequest = { expanded.value= false }
+        ) {
+            DropdownMenuItem(
+                text = { Text("Chaussures") },
+                onClick = { expanded.value= false }
             )
-        )
-
-        Spacer(modifier = Modifier.width(8.dp))
-
-        FilterChip(
-            selected = true,
-            onClick = {},
-            label = {
-                Text("Accessoires")
-            },
-            colors = FilterChipDefaults.filterChipColors(
-                selectedContainerColor = Color(0XFF1C69C0),
-                selectedLabelColor = Color.White,
-                containerColor = Color.LightGray,
-                labelColor = Color.Black
+            DropdownMenuItem(
+                text = { Text("Vêtements") },
+                onClick = { expanded.value= false }
             )
-        )
+            DropdownMenuItem(
+                text = { Text("Accessoires") },
+                onClick = { expanded.value= false }
+            )
+        }
     }
+}
+
+@Composable
+fun Filterchip(name: String) {
+    FilterChip(
+        selected = true,
+        onClick = {},
+        label = { Text(name) },
+        colors = FilterChipDefaults.filterChipColors(
+            selectedContainerColor = Color(0XFF1C69C0),
+            selectedLabelColor = Color.White,
+            containerColor = Color.LightGray,
+            labelColor = Color.Black
+        )
+    )
 }
--- app/src/main/java/com/example/myapplication/UIs/HomePage.kt
+++ app/src/main/java/com/example/myapplication/UIs/HomePage.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material3.BottomAppBar
@@ -20,6 +21,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
@@ -132,7 +134,7 @@ fun  HomePage(navController: NavController) {
     ){ paddingValues ->
 
         Column(
-            modifier = Modifier.padding(paddingValues).fillMaxSize(),
+            modifier = Modifier.padding(paddingValues).fillMaxSize().padding(horizontal = 8.dp),
 
 
         ) {
@@ -158,11 +160,18 @@ fun  HomePage(navController: NavController) {
                 modifier = Modifier
                     .fillMaxWidth()
                     .height(55.dp)
-                    .padding(horizontal = 20.dp)
             )
             Spacer(modifier = Modifier.height(20.dp))
-            FilterSection()
-            Spacer(modifier = Modifier.height(8.dp))
+            //Row {
+                FilterSection()
+//                Spacer(modifier = Modifier.width(8.dp))
+//                IconButton(onClick = {}) {
+//                    Icon(
+//                        imageVector = Icons.Default.MoreVert,
+//                        contentDescription = "Plus de filtres"
+//                    )
+//                }
+           // }
             GridLazyColumn(
                 onAddToCart = { product ->
                     cart.value = cart.value + product
